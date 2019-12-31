66aec53b755c07a446d32f6b8f06d3844366dca2
joey_github <54054753+514870106@users.noreply.github.com>
2019-12-31 18:00:38 +0800

    提交

--- web/src/components/login_phone.vue
+++ web/src/components/login_phone.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="login_phone">
-    <el-form :model="form_msg" :rules="rule" lazy=true;>
-      <el-form-item prop="Phone">
-        <el-input v-model.number="form_msg.Phone"  placeholder="手机号"></el-input>
+    <el-form :model="form_msg" :rules="rule" lazy="true" ref="form_msg">
+      <el-form-item prop="phone">
+        <el-input v-model.number="form_msg.phone" placeholder="手机号"></el-input>
       </el-form-item>
-      <el-form-item prop="Password">
-        <el-input type="password" v-model="form_msg.Password" placeholder="密码"></el-input>
+      <el-form-item prop="password">
+        <el-input type="password" v-model="form_msg.password" placeholder="密码"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="login_phone">登录</el-button>
+        <el-button type="primary" @click="login_phone('form_msg')">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -16,29 +16,41 @@
 
 <script>
 export default {
-  name:"login_phone",
+  name: "login_phone",
   data() {
     return {
       form_msg: {
-        Phone: "",
-        Password: ""
+        phone: "",
+        password: ""
       },
       rule: {
-        Phone: [
-          { required: true, message: "请输入手机号", trigger: ['blur', 'change'] },
-          { type:"number" , message: "请输入数字", trigger: ['blur', 'change'] }
-          ],
-        Password: [
-          { required: true, message: "请输入密码", trigger: ['blur', 'change'] },
-          ]
+        phone: [
+          {
+            required: true,
+            message: "请输入手机号",
+            trigger: ["blur", "change"]
+          },
+          { type: "number", message: "请输入数字", trigger: ["blur", "change"] }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: ["blur", "change"] }
+        ]
       }
     };
   },
   methods: {
-      login_phone() {
-        window.console.log(JSON.stringify(this.form_msg));
-      }
+    //登录事件，表单验证通过后请求登录接口
+    login_phone(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          window.console.log(JSON.stringify(this.form_msg));
+        } else {
+          window.console.log("error submit!!");
+          return false;
+        }
+      });
     }
+  }
 };
 </script>
 
